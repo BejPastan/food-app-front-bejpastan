8e502a48821969f3704733fc00c7afc2db6db332
BejPastan <s.synaszko@gmail.com>
2026-08-03 00:25:27 +0200

    fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import mkcert from 'vite-plugin-mkcert'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), mkcert()],
-  server: {
-    https: true // Enables HTTPS automatically with trusted certs
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
